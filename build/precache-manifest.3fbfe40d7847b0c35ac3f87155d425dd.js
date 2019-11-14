f16a6f6c9d381df96a5a3114577d10af8ebd9262
Felipe Silva <felipe.florianopolis@gmail.com>
2019-11-14 19:48:56 -0300

    Test2

---
+++ build/precache-manifest.3fbfe40d7847b0c35ac3f87155d425dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6772f74d2a318e824babeeeea2cc533d",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "2c5d502d10d106e3dce7",
+    "url": "/test/static/css/main.9bd49755.chunk.css"
+  },
+  {
+    "revision": "332faf71d7f3433b806e",
+    "url": "/test/static/js/2.af6a2145.chunk.js"
+  },
+  {
+    "revision": "2c5d502d10d106e3dce7",
+    "url": "/test/static/js/main.592c4d69.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  }
+]);
